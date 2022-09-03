503140fb982ac9b4564760290072aa959084da48
Amangeldy Tomiris <70026328+Tomiris-Aman@users.noreply.github.com>
2022-09-03 19:22:23 +0600

    v.1.1

--- js/main.js
+++ js/main.js
@@ -1,23 +1,23 @@
 $(document).ready(function(){
     $('.showcase-inner').slick({
+        infinite: true,
         slidesToShow: 3,
         slidesToScroll: 2,
         autoplay: true,
         autoplaySpeed: 1000,
         dots: true,
-        infinite: true,
         centerMode:true,
-    });
+    });        
   });
 
   $(document).ready(function(){
     $('.reviews-inner').slick({
+        infinite: true,
         slidesToShow: 1,
         slidesToScroll: 1,
         autoplay: true,
         autoplaySpeed: 3000,
         dots: true,
-        infinite: true,
         centerMode:true,
         arrows: false,
     });
